2688277be1b1dcaf846c939521c4ea97b01a83e0
Igor Queiroz <queirozigor100@gmail.com>
2024-03-04 10:26:37 -0300

    entrada do usuário

--- campo-minado.c
+++ campo-minado.c
@@ -10,6 +10,7 @@ typedef struct campo{
 
 int menuPrincipal();
 void desenharCampo(Campo campo_minado[][10]);
+void verBombas(Campo campo_minado[][10]);
 
 int main(void){
 
@@ -17,8 +18,11 @@ int main(void){
     int opcao;
     int i, j;
     int eixo_bomba_x, eixo_bomba_y;
-    opcao = menuPrincipal();
+    int eixo_jogador_x, eixo_jogador_y;
+    int valid;
 
+
+    opcao = menuPrincipal();
     switch (opcao){
     case 1:
         for(i=0; i<10; i++){
@@ -39,12 +43,57 @@ int main(void){
         for(i=0; i<10; i++){
             for(j=0; j<10; j++){
                 if(campo_minado[i][j].bomba == 1){
-                    
+                    if(i+1 <= 9){
+                        campo_minado[i+1][j].perigo++;
+                    }
+                    if(i-1 >= 0){
+                        campo_minado[i-1][j].perigo++;
+                    }
+                    if(j+1 <= 9){
+                        campo_minado[i][j+1].perigo++;
+                    }
+                    if(j-1 >= 0){
+                        campo_minado[i][j-1].perigo++;
+                    }
+                    if(i-1 >= 0 && j-1 >= 0){
+                        campo_minado[i-1][j-1].perigo++;
+                    }
+                    if(i-1 >= 0 && j+1 <= 9){
+                        campo_minado[i-1][j+1].perigo++;
+                    }
+                    if(i+1 <= 9 && j-1 >= 0){
+                        campo_minado[i+1][j-1].perigo++;
+                    }
+                    if(i+1 <= 9 && j+1 <= 9){
+                        campo_minado[i+1][j+1].perigo++;
+                    }
                 }
             }
         }
 
-        desenharCampo(campo_minado);
+        while(1){
+            desenharCampo(campo_minado);
+            printf("\n");
+            printf("Informe o eixo X: ");  
+            valid = 0;
+            while(valid == 0){
+                valid = scanf("%d",&eixo_jogador_x);
+                while(getchar() != '\n');
+                if(valid == 0){
+                    printf("Por favor, insira um valor entre 1 e 10\n");
+                }
+            }
+            printf("Informe o eixo Y: ");  
+            valid = 0;
+            while(valid == 0){
+                valid = scanf("%d",&eixo_jogador_y);
+                while(getchar() != '\n');
+                if(valid == 0){
+                    printf("Por favor, insira um valor entre 1 e 10\n");
+                }
+            }
+        } 
+
         break;
     case 2:
         exit(0);
@@ -95,3 +144,32 @@ void desenharCampo(Campo campo_minado[][10]){
     }
 }
 
+
+
+
+/* Função auxiliar */
+void verBombas(Campo campo_minado[][10]){
+    int i, j;
+    for(i=0; i<20; i++){
+        printf("--");
+    }
+    printf("\n");
+    for(i=0; i<10; i++){
+        for(j=0; j<10; j++){
+            printf("|");
+            if(campo_minado[i][j].aberto == 0){
+                if(campo_minado[i][j].bomba == 1){
+                    printf(" * ");
+                }
+                else{
+                    printf(" %d ", campo_minado[i][j].perigo);
+                }
+            }
+        }
+        printf("|");
+        printf("\n");
+    }
+    for(i=0; i<20; i++){
+        printf("--");
+    }
+}
