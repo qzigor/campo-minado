764a7a950f296fe006d4deba2eb07eb0fb2f3a8e
Igor Queiroz <queirozigor100@gmail.com>
2024-03-10 09:20:02 -0300

    added the flags in the minefield

--- campo-minado.c
+++ campo-minado.c
@@ -2,110 +2,145 @@
 #include<stdlib.h>
 #include<time.h>
 
-typedef struct campo{
-    int aberto;
-    int bomba;
-    int perigo;
-} Campo;
+typedef struct field{
+    int open;
+    int bomb;
+    int danger;
+    int flag;
+} Field;
 
-int menuPrincipal();
-void desenharCampo(Campo campo_minado[][10]);
-void iniciarCampo(Campo (*campo_minado)[10]);
-void adicionarBombas(Campo (*campo_minado)[10]);
-void adicionarIndices(Campo (*campo_minado)[10]);
-int temBomba(Campo (*campo_minado)[10], int eixo_x, int eixo_y);
-void abrirCampos(Campo (*campo_minado)[10], int eixo_x, int eixo_y, int *vitoria);
+int mainMenu();
+int auxiliaryMenu();
+void drawField(Field mine_field[][10]);
+void startField(Field (*mine_field)[10]);
+void addBombs(Field (*mine_field)[10]);
+void addIndices(Field (*mine_field)[10]);
+int axis();
+int isBombs(Field (*mine_field)[10], int eixo_x, int eixo_y);
+void openFields(Field (*mine_field)[10], int eixo_x, int eixo_y, int *vitoria);
+void addFlag(Field (*mine_field)[10], int eixo_x, int eixo_y);
+void removeFlag(Field (*mine_field)[10], int eixo_x, int eixo_y);
 
-void verCampoMinado(Campo campo_minado[][10]);
+void viewField(Field mine_field[][10]);
 
 
 int main(void){
 
-    Campo campo_minado[10][10];
-    int opcao;
-    int eixo_jogador_x = -1, eixo_jogador_y = -1;
-    int valid;
-    int tem_bomba;
-    int qnt_vitoria = 0;
+    Field mine_field[10][10];
+    int option;
+    int axis_player_x = -1, axis_player_y = -1;
+    int validator;
+    int quantity_for_victory = 0;
+    int game = 1;
+    int play = 0;
 
-    opcao = menuPrincipal();
+    option = mainMenu();
+    if(option == 2){
+        exit('0');
+    }
+
     system("cls");
-    switch (opcao){
-    case 1:
-
-        iniciarCampo(campo_minado);
-        adicionarBombas(campo_minado);
-        adicionarIndices(campo_minado);
+    while(game == 1){
+        startField(mine_field);
+        addBombs(mine_field);
+        addIndices(mine_field);
 
         while(1){
-            printf(" qnt_vitoria: %d\n", qnt_vitoria);
-            desenharCampo(campo_minado);
+            drawField(mine_field);
             printf("\n");
-            verCampoMinado(campo_minado);
+            viewField(mine_field);
             printf("\n");
-            printf("Informe o eixo X: ");  
-            valid = 0;
-            while(valid == 0 || eixo_jogador_x <= 0 || eixo_jogador_x > 10){
-                valid = scanf("%d", &eixo_jogador_x);
+            validator = 0;
+            while(validator == 0 || play < 1 || play > 3){
+                printf("Qual opcao deseja:\n");
+                printf("[1] - abrir um campo.\n");
+                printf("[2] - colocar bandeira\n");
+                printf("[3] - remover bandeira\n");
+                validator = scanf("%d", &play);
                 while(getchar() != '\n');
-                if(valid == 0 || eixo_jogador_x <= 0 || eixo_jogador_x > 10){
-                    printf("Por favor, insira um valor entre 1 e 10.\n");
-                    printf("Informe o eixo X: ");
+                if(validator == 0 || play < 1 || play > 3){
+                    printf("Por favor, selecione uma das 3 opcoes.\n");
                 }
             }
-            eixo_jogador_x--;
-            printf("Informe o eixo Y: ");  
-            valid = 0;
-            while(valid == 0 || eixo_jogador_y<= 0 || eixo_jogador_y > 10){
-                valid = scanf("%d", &eixo_jogador_y);
-                while(getchar() != '\n');
-                if(valid == 0 || eixo_jogador_y <= 0 || eixo_jogador_y > 10){
-                    printf("Por favor, insira um valor entre 1 e 10.\n");
-                    printf("Informe o eixo Y: ");
+            if(play == 1){
+                printf("Informe o eixo X: ");  
+                axis_player_x = axis();
+                printf("Informe o eixo Y: ");  
+                axis_player_y = axis();
+                if(isBombs(mine_field,axis_player_x,axis_player_y) == 1){
+                    printf("Voce perdeu!!!");
+                    
+                }
+                if(isBombs(mine_field,axis_player_x,axis_player_y) == 0){
+                    openFields(mine_field, axis_player_x, axis_player_y, &quantity_for_victory);
+                    system("cls");
+                }
+                if(quantity_for_victory == 88){
+                    system("cls");
+                    printf("Parabens!!! Voce venceu o jogo.");
                 }
             }
-            eixo_jogador_y--;
-            tem_bomba = temBomba(campo_minado,eixo_jogador_x,eixo_jogador_y);
-            if(tem_bomba == 1){
-                printf("Voce perdeu!!!");
-            }
-            if(tem_bomba == 0){
-                abrirCampos(campo_minado, eixo_jogador_x, eixo_jogador_y, &qnt_vitoria);
+            if(play == 2){
+                printf("Informe o eixo X: ");  
+                axis_player_x = axis();
+                printf("Informe o eixo Y: ");  
+                axis_player_y = axis();
+                addFlag(mine_field, axis_player_x, axis_player_y);
                 system("cls");
             }
-            if(qnt_vitoria == 88){
+            if(play == 3){
+                printf("Informe o eixo X: ");  
+                axis_player_x = axis();
+                printf("Informe o eixo Y: ");  
+                axis_player_y = axis();
+                removeFlag(mine_field, axis_player_x, axis_player_y);
                 system("cls");
-                printf("Parabens!!! Voce venceu o jogo.");
-            }
+            }         
         } 
-
-        break;
-    case 2:
-        exit(0);
-        break;
     }
+
 }
 
-int menuPrincipal(){
-    int valid = 0;
-    int opcao;
+int mainMenu(){
+    int validator = 0;
+    int option;
 
-    printf("\nBem-vindo ao jogo campo minado.\n");
+    printf("\nBem-vindo ao jogo Field minado.\n");
     printf("Selecione uma opcao: \n");
     printf("[1] - Novo jogo.\n");
     printf("[2] - Sair do jogo.\n");
-    while(valid == 0){
-        valid = scanf("%d", &opcao);
+    while(validator == 0){
+        validator = scanf("%d", &option);
         while(getchar() != '\n');
-        if(valid == 0){
+        if(validator == 0){
             printf("Por favor, digite um numero de uma opcao valida.\n");
         }
     }
     
-    return opcao;
+    return option;
 }
 
-void desenharCampo(Campo campo_minado[][10]){
+int auxiliaryMenu(){
+    int validator = 0;
+    int option_usuer;
+
+    printf("Selecione uma opcao: \n");
+    printf("[1] - Novo jogo.\n");
+    printf("[2] - Sair do jogo.\n");
+    printf("[3] - Voltar para o menu principal.\n");
+    while(validator == 0){
+        validator = scanf("%d", &option_usuer);
+        while(getchar() != '\n');
+        if(validator == 0){
+            printf("Por favor, digite um numero de uma opcao valida.\n");
+        }
+    }
+    
+    return option_usuer;
+}
+
+
+void drawField(Field mine_field[][10]){
     int i, j;
     printf("   ");
     for(i=0; i<10; i++){
@@ -126,11 +161,14 @@ void desenharCampo(Campo campo_minado[][10]){
         }
         for(j=0; j<10; j++){
             printf("|");
-            if(campo_minado[i][j].aberto == 0){
+            if(mine_field[i][j].open == 0 && mine_field[i][j].flag == 1){
+                printf(" ! ");
+            }
+            if(mine_field[i][j].open == 0 && mine_field[i][j].flag == 0){
                 printf("   ");
             }
-            if(campo_minado[i][j].aberto == 1){
-                printf(" %d ", campo_minado[i][j].perigo);
+            if(mine_field[i][j].open == 1){
+                printf(" %d ", mine_field[i][j].danger);
             }
         }
         printf("|");
@@ -142,67 +180,82 @@ void desenharCampo(Campo campo_minado[][10]){
     }
 }
 
-void iniciarCampo(Campo (*campo_minado)[10]){
+void startField(Field (*mine_field)[10]){
     int i, j;
     for(i=0; i<10; i++){
             for(j=0; j<10; j++){
-                campo_minado[i][j].aberto = 0;
-                campo_minado[i][j].bomba = 0;
-                campo_minado[i][j].perigo = 0;
+                mine_field[i][j].open = 0;
+                mine_field[i][j].bomb = 0;
+                mine_field[i][j].danger = 0;
+                mine_field[i][j].flag = 0;
             }
         }
 }
 
-void adicionarBombas(Campo (*campo_minado)[10]){
-    int eixo_bomba_x, eixo_bomba_y;
-    int qnt_bombas = 0;
+void addBombs(Field (*mine_field)[10]){
+    int axis_bomb_x, axis_bomb_y;
+    int quantity_bomb = 0;
     srand(time(NULL));
-    while(qnt_bombas < 12){
-        eixo_bomba_x = rand() % 10;
-        eixo_bomba_y = rand() % 10;
-        if(campo_minado[eixo_bomba_x][eixo_bomba_y].bomba == 0){
-            campo_minado[eixo_bomba_x][eixo_bomba_y].bomba = 1;
-            qnt_bombas++;
+    while(quantity_bomb < 12){
+        axis_bomb_x = rand() % 10;
+        axis_bomb_y = rand() % 10;
+        if(mine_field[axis_bomb_x][axis_bomb_y].bomb == 0){
+            mine_field[axis_bomb_x][axis_bomb_y].bomb = 1;
+            quantity_bomb++;
         }
     }
 }
 
-void adicionarIndices(Campo (*campo_minado)[10]){
+void addIndices(Field (*mine_field)[10]){
     int i, j;
     for(i=0; i<10; i++){
         for(j=0; j<10; j++){
-            if(campo_minado[i][j].bomba == 1){
+            if(mine_field[i][j].bomb == 1){
                 if(i+1 <= 9){
-                    campo_minado[i+1][j].perigo++;
+                    mine_field[i+1][j].danger++;
                 }
                 if(i-1 >= 0){
-                    campo_minado[i-1][j].perigo++;
+                    mine_field[i-1][j].danger++;
                 }
                 if(j+1 <= 9){
-                    campo_minado[i][j+1].perigo++;
+                    mine_field[i][j+1].danger++;
                 }
                 if(j-1 >= 0){
-                    campo_minado[i][j-1].perigo++;
+                    mine_field[i][j-1].danger++;
                 }
                 if(i-1 >= 0 && j-1 >= 0){
-                    campo_minado[i-1][j-1].perigo++;
+                    mine_field[i-1][j-1].danger++;
                 }
                 if(i-1 >= 0 && j+1 <= 9){
-                    campo_minado[i-1][j+1].perigo++;
+                    mine_field[i-1][j+1].danger++;
                 }
                 if(i+1 <= 9 && j-1 >= 0){
-                    campo_minado[i+1][j-1].perigo++;
+                    mine_field[i+1][j-1].danger++;
                 }
                 if(i+1 <= 9 && j+1 <= 9){
-                    campo_minado[i+1][j+1].perigo++;
+                    mine_field[i+1][j+1].danger++;
                 }
             }
         }
     }
 }
 
-int temBomba(Campo (*campo_minado)[10], int eixo_x, int eixo_y){
-    if(campo_minado[eixo_x][eixo_y].bomba == 1){
+int axis(){
+    int validator = 0;
+    int axis_player = 0;
+    while(validator == 0 || axis_player <= 0 || axis_player > 10){
+        validator = scanf("%d", &axis_player);
+        while(getchar() != '\n');
+        if(validator == 0 || axis_player <= 0 || axis_player > 10){
+            printf("Por favor, insira um valor entre 1 e 10.\n");
+        }
+    }
+    axis_player--;
+    return axis_player;
+}
+
+int isBombs(Field (*mine_field)[10], int eixo_x, int eixo_y){
+    if(mine_field[eixo_x][eixo_y].bomb == 1){
         return 1;
     }
     else{
@@ -210,44 +263,52 @@ int temBomba(Campo (*campo_minado)[10], int eixo_x, int eixo_y){
     }
 }
 
-void abrirCampos(Campo (*campo_minado)[10], int eixo_x, int eixo_y, int *vitoria){
+void openFields(Field (*mine_field)[10], int eixo_x, int eixo_y, int *vitoria){
     *vitoria = *vitoria + 1;
-    campo_minado[eixo_x][eixo_y].aberto = 1;
-    if(campo_minado[eixo_x][eixo_y].perigo != 0){
+    mine_field[eixo_x][eixo_y].open = 1;
+    if(mine_field[eixo_x][eixo_y].danger != 0){
         return;
     }
     else{
-        if(eixo_x+1 <= 9 && campo_minado[eixo_x+1][eixo_y].aberto == 0){
-            abrirCampos(campo_minado, eixo_x+1, eixo_y, vitoria);
+        if(eixo_x+1 <= 9 && mine_field[eixo_x+1][eixo_y].open == 0){
+            openFields(mine_field, eixo_x+1, eixo_y, vitoria);
         }
-        if(eixo_x-1 >= 0 && campo_minado[eixo_x-1][eixo_y].aberto == 0){
-            abrirCampos(campo_minado, eixo_x-1, eixo_y, vitoria);
+        if(eixo_x-1 >= 0 && mine_field[eixo_x-1][eixo_y].open == 0){
+            openFields(mine_field, eixo_x-1, eixo_y, vitoria);
         }
-        if(eixo_y+1 <= 9 && campo_minado[eixo_x][eixo_y+1].aberto == 0){
-            abrirCampos(campo_minado, eixo_x, eixo_y+1, vitoria);
+        if(eixo_y+1 <= 9 && mine_field[eixo_x][eixo_y+1].open == 0){
+            openFields(mine_field, eixo_x, eixo_y+1, vitoria);
         }
-        if(eixo_y-1 >= 0 && campo_minado[eixo_x][eixo_y-1].aberto == 0){
-            abrirCampos(campo_minado, eixo_x, eixo_y-1, vitoria);
+        if(eixo_y-1 >= 0 && mine_field[eixo_x][eixo_y-1].open == 0){
+            openFields(mine_field, eixo_x, eixo_y-1, vitoria);
         }
-        if(eixo_x-1 >= 0 && eixo_y-1 >= 0 && campo_minado[eixo_x-1][eixo_y-1].aberto == 0){
-            abrirCampos(campo_minado, eixo_x-1, eixo_y-1, vitoria);
+        if(eixo_x-1 >= 0 && eixo_y-1 >= 0 && mine_field[eixo_x-1][eixo_y-1].open == 0){
+            openFields(mine_field, eixo_x-1, eixo_y-1, vitoria);
         }
-        if(eixo_x-1 >= 0 && eixo_y+1 <= 9 && campo_minado[eixo_x-1][eixo_y+1].aberto == 0){
-            abrirCampos(campo_minado, eixo_x-1, eixo_y+1, vitoria);
+        if(eixo_x-1 >= 0 && eixo_y+1 <= 9 && mine_field[eixo_x-1][eixo_y+1].open == 0){
+            openFields(mine_field, eixo_x-1, eixo_y+1, vitoria);
         }
-        if(eixo_x+1 <= 9 && eixo_y-1 >= 0 && campo_minado[eixo_x+1][eixo_y-1].aberto == 0){
-            abrirCampos(campo_minado, eixo_x+1, eixo_y-1, vitoria);
+        if(eixo_x+1 <= 9 && eixo_y-1 >= 0 && mine_field[eixo_x+1][eixo_y-1].open == 0){
+            openFields(mine_field, eixo_x+1, eixo_y-1, vitoria);
         }
-        if(eixo_x+1 <= 9 && eixo_y+1 <= 9 && campo_minado[eixo_x+1][eixo_y+1].aberto == 0){
-            abrirCampos(campo_minado, eixo_x+1, eixo_y+1, vitoria);
+        if(eixo_x+1 <= 9 && eixo_y+1 <= 9 && mine_field[eixo_x+1][eixo_y+1].open == 0){
+            openFields(mine_field, eixo_x+1, eixo_y+1, vitoria);
         }
     }
 
 }
 
+void addFlag(Field (*mine_field)[10], int eixo_x, int eixo_y){
+    mine_field[eixo_x][eixo_y].flag = 1;
+}
 
-/* Função auxiliar */
-void verCampoMinado(Campo campo_minado[][10]){
+void removeFlag(Field (*mine_field)[10], int eixo_x, int eixo_y){
+    mine_field[eixo_x][eixo_y].flag = 0;
+}
+
+
+/* auxiliary function */
+void viewField(Field mine_field[][10]){
     int i, j;
     for(i=0; i<20; i++){
         printf("--");
@@ -256,11 +317,11 @@ void verCampoMinado(Campo campo_minado[][10]){
     for(i=0; i<10; i++){
         for(j=0; j<10; j++){
             printf("|");
-                if(campo_minado[i][j].bomba == 1){
+                if(mine_field[i][j].bomb == 1){
                     printf(" \033[1;31m*\033[0m ");
                 }
                 else{
-                    printf(" %d ", campo_minado[i][j].perigo);
+                    printf(" %d ", mine_field[i][j].danger);
                 }
         }
         printf("|");
